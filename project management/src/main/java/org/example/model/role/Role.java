d699a1f499270f3c2236f06e030c045496dc888b
Rafał Sochacki <rafal.sochacki@ttpsc.pl>
2024-01-29 20:48:58 +0100

    user and role models

---
+++ project management/src/main/java/org/example/model/role/Role.java
@@ -0,0 +1,8 @@
+package org.example.model.role;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    HR,
+    ENGINEER
+}
--- project management/src/main/java/org/example/model/user/User.java
+++ project management/src/main/java/org/example/model/user/User.java
@@ -1,10 +1,47 @@
 package org.example.model.user;
 
+import org.example.model.project.Project;
+import org.example.model.role.Role;
+
+import java.util.Map;
+import java.util.Set;
+
 public class User {
-    int id;
-    String username;
+    private int id;
+    private String username;
+    private Map<Project, Set<Role>> rolesOnProjects;
 
     public User(String username) {
         this.username = username;
+    }
+
+    public User(int id, String username, Map<Project, Set<Role>> rolesOnProjects) {
+        this.id = id;
+        this.username = username;
+        this.rolesOnProjects = rolesOnProjects;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Map<Project, Set<Role>> getRolesOnProjects() {
+        return rolesOnProjects;
+    }
+
+    public void setRolesOnProjects(Map<Project, Set<Role>> rolesOnProjects) {
+        this.rolesOnProjects = rolesOnProjects;
     }
 }
